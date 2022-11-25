aae221cfe28f2f38b1bbf9c845ebc35acc4b3678
suhailbehlim <behlimsuhail@gmail.com>
2022-11-25 15:38:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 
 function App() {
+  const position = [51.505, -0.09]
+
   return (
-    <div className="App">
-     <h1>my first leafet map </h1>
-    </div>
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    {/* <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker> */}
+  </MapContainer>
   );
 }
 
